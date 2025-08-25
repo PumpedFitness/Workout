770953ad548ef71fd62f5ec0ce96d5feadc37e8c
devin <devin@Mac.localdomain>
2025-08-25 12:50:42 +0200

    Removed old Validators

--- src/main/kotlin/configuration/Routing.kt
+++ src/main/kotlin/configuration/Routing.kt
@@ -1,22 +1,13 @@
 package ord.pumped.configuration
 
-import dev.nesk.akkurate.ktor.server.registerValidator
 import io.ktor.server.application.*
 import io.ktor.server.plugins.requestvalidation.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.resources.*
 import ord.pumped.common.APIException
-import ord.pumped.usecase.user.rest.request.validateUpdateProfileRequest
-import ord.pumped.usecase.user.rest.request.validateUserLoginRequest
-import ord.pumped.usecase.user.rest.request.validateUserRegisterRequest
-import ord.pumped.usecase.user.rest.request.validateUserUpdatePasswordRequest
 
 fun Application.configureRouting() {
     install(RequestValidation) {
-        registerValidator(validateUserRegisterRequest)
-        registerValidator(validateUserLoginRequest)
-        registerValidator(validateUpdateProfileRequest)
-        registerValidator(validateUserUpdatePasswordRequest)
     }
     install(Resources)
 
