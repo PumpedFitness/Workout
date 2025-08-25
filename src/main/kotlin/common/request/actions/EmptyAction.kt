8630f312a69e9db6caa7871ba675fc18a985a825
devin <devin@Mac.localdomain>
2025-08-25 12:58:43 +0200

    Removed old files

--- src/main/kotlin/common/request/actions/EmptyAction.kt
+++
@@ -1,7 +0,0 @@
-package org.pumped.common.request.actions
-
-import kotlinx.serialization.Serializable
-import org.pumped.io.websocket.routing.messaging.IWebsocketAction
-
-@Serializable
-data class EmptyAction(val empty: String = ""): IWebsocketAction
--- src/main/kotlin/configuration/Monitoring.kt
+++ src/main/kotlin/configuration/Monitoring.kt
@@ -1,15 +1,12 @@
 package org.pumped.configuration
 
 import io.ktor.server.application.*
-import org.pumped.usecase.user.persistence.dto.UsersTable
-import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
 
 fun Application.pingDatabase(): Boolean {
     log.info("Pinging database...")
     return try {
         transaction {
-            UsersTable.selectAll()
         }
         true
     } catch (_: Throwable) {
