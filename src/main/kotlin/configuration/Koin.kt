47a364a4ab6ad6a8c55cd1464000d2b256b1f964
devin <devin@Mac.localdomain>
2025-08-25 15:58:41 +0200

    Fixed build

--- src/main/kotlin/configuration/Koin.kt
+++ src/main/kotlin/configuration/Koin.kt
@@ -1,13 +1,12 @@
 package org.pumped.configuration
 
 import io.ktor.server.application.*
-import org.pumped.common.security.service.securityModule
 import org.koin.ktor.plugin.Koin
 import org.koin.logger.slf4jLogger
 
 fun Application.configureKoin() {
     install(Koin) {
         slf4jLogger()
-        modules(securityModule)
+        modules()
     }
 }
